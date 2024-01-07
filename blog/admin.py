53efb1aa9bb09d3bc7f796358a6415bf3d78c904
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-07 15:45:45 +0330

    define Article actions

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
+# Admin header change
+admin.site.site_header = "University Blog"
+
+
 # Register your models here.
+def make_published(modeladmin, request, queryset):
+    rows_updated = queryset.update(status='p')
+    message_bit = "Published"
+    modeladmin.message_user(request, "{} article {}".format(rows_updated, message_bit))
+
+
+make_published.short_description = "Publish selected articles"
+
+
+def make_draft(modeladmin, request, queryset):
+    rows_updated = queryset.update(status='d')
+    message_bit = "Drafted"
+    modeladmin.message_user(request, "{} article {}".format(rows_updated, message_bit))
+
+
+make_draft.short_description = "Draft selected articles"
