588be86d5e4f5a1b82405517f908748cbb453186
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-08 00:39:43 +0330

    add Home view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from datetime import datetime, timedelta
+
+from django.db.models import Count, Q
+from django.views.generic import ListView
+
+from blog.models import Article
+
 
 # Create your views here.
+class Home(ListView):
+    queryset = Article.objects.published()
+    paginate_by = 5
+    template_name = 'blog/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        last_month = datetime.today() - timedelta(days=30)
+        context['month_popular_articles'] = Article.objects.published().annotate(
+            count=Count('hits', filter=Q(articlehit__created__gt=last_month))) \
+                                                .order_by('-count', '-publish')[:6]
+        return context
