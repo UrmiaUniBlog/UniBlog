6f7897c619d4af47249b1f3b0fdffc67d432fcdf
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-07 15:13:58 +0330

    add Category model

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    parent = models.ForeignKey('self', default=None, null=True, blank=True, on_delete=models.SET_NULL,
+                               related_name='children')
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+    status = models.BooleanField(default=True)
+    position = models.IntegerField()
+
+    class Meta:
+        verbose_name = "Category"
+        verbose_name_plural = "Categories"
+        ordering = ['parent__id', 'position']
+
+    def __str__(self):
+        return self.title
