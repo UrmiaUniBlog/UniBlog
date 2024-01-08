015d6a86540d4a099c41e0ad0f40103afb0af3ca
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-08 14:47:31 +0330

    add ArticleDetail view

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,10 @@
 from datetime import datetime, timedelta
 
 from django.db.models import Count, Q
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404, redirect
+from django.views.generic import ListView, DetailView
 
-from blog.models import Article
+from blog.models import Article, Comment
 
 
 # Create your views here.
@@ -33,3 +34,26 @@ class PopularList(ListView):
     def get_queryset(self):
         return Article.objects.published().annotate(count=Count('hits', filter=Q(hits__gt=0))) \
             .order_by('-count', '-publish')
+
+
+class ArticleDetail(DetailView):
+    def get_object(self):
+        slug = self.kwargs.get('slug')
+        article = get_object_or_404(Article.objects.published(), slug=slug)
+
+        ip_address = self.request.user.ip_address
+        if ip_address not in article.hits.all():
+            article.hits.add(ip_address)
+
+        return article
+
+    def post(self, request, slug):
+        article = get_object_or_404(Article.objects.published(), slug=slug)
+        parent_id = request.POST.get('parent_id')
+        body = request.POST.get('body')
+        if parent_id == '':
+            Comment.objects.create(article=article, body=body, user=request.user)
+        else:
+            Comment.objects.create(article=article, body=body, user=request.user, parent_id=int(parent_id))
+
+        return redirect(self.request.path_info + '#comments')
