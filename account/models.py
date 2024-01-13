688861c5943b63be00a4d18adf41d405e9da0a19
Romihkh <romi.hemmati.kh.02@gmail.com>
2024-01-14 00:36:26 +0330

    add User model(override AbstractUser model)

--- account/models.py
+++ account/models.py
@@ -1,3 +1,19 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    email = models.EmailField(unique=True, verbose_name='Email')
+
+    is_author = models.BooleanField(default=False, verbose_name="Author status")
+    special_user = models.DateTimeField(default=timezone.now, verbose_name="Subscription expires in")
+
+    def is_special_user(self):
+        if self.special_user > timezone.now():
+            return True
+        else:
+            return False
+
+    is_special_user.boolean = True
+    is_special_user.short_description = "Special user status"
