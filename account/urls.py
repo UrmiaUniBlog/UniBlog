3b2c2288dd037fca6eadb92afc175bcad1172dc4
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-14 10:41:59 +0330

    add CommentList view

--- account/urls.py
+++ account/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from account.views import ArticleList, ArticleCreate
+from account.views import ArticleList, ArticleCreate, CommentList
 
 app_name = 'account'
 
 urlpatterns = [
     path('', ArticleList.as_view(), name='home'),
     path('article/new', ArticleCreate.as_view(), name='article-create'),
+    path('comments/', CommentList.as_view(), name='comment-list'),
 ]
