9f237b8b85438e83bdf27c64f88b306afd87b347
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-14 10:50:59 +0330

    add CommentUpdate view

--- account/urls.py
+++ account/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from account.views import ArticleList, ArticleCreate, CommentList
+from account.views import ArticleList, ArticleCreate, CommentList, CommentUpdate
 
 app_name = 'account'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', ArticleList.as_view(), name='home'),
     path('article/new', ArticleCreate.as_view(), name='article-create'),
     path('comments/', CommentList.as_view(), name='comment-list'),
+    path('comments/<int:pk>', CommentUpdate.as_view(), name='comment-update'),
 ]
