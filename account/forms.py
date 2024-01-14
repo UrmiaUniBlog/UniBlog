c8d3a296ce4dd0e637e988a5d65a6180875771fe
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-14 13:01:32 +0330

    add ProfileForm

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,8 @@
+import datetime
+
 from django import forms
 
+from account.models import User
 from blog.models import Comment
 
 
@@ -17,3 +20,33 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('user', 'article', 'body', 'status')
+
+
+class ProfileForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        user = kwargs.pop('user')
+
+        super(ProfileForm, self).__init__(*args, **kwargs)
+
+        remaining_days = (user.special_user - datetime.datetime.now(datetime.timezone.utc)).days
+        if remaining_days > 0:
+            self.fields['special_user'].help_text = f'Your subscription expires in {remaining_days} days.'
+        else:
+            self.fields['special_user'].help_text = f'Your subscription has expired.'
+        self.fields['is_author'].help_text = "Designates whether this user should be treated as author."
+        self.fields['is_staff'].help_text = "Designates whether this user is authorized to access privileged sections"
+
+        if not user.is_superuser:
+            self.fields['username'].help_text = None
+            self.fields['username'].disabled = True
+            self.fields['special_user'].disabled = True
+            self.fields['is_author'].disabled = True
+            self.fields['is_active'].disabled = True
+            self.fields['is_superuser'].disabled = True
+            self.fields['is_staff'].disabled = True
+            self.fields['last_login'].disabled = True
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'first_name', 'last_name',
+                  'special_user', 'is_author', 'is_superuser', 'is_staff', 'is_active', 'last_login']
