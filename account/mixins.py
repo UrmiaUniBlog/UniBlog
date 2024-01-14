211499618fa0cc84aeb1c2da493e84d8604454e4
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-14 10:34:27 +0330

    define Fields & FormValid mixins

--- account/mixins.py
+++ account/mixins.py
@@ -9,3 +9,27 @@ class AuthorizedAccessMixin():
             return super().dispatch(request, *args, **kwargs)
         else:
             return redirect('account:profile')
+
+
+class FieldsMixin():
+    def dispatch(self, request, *args, **kwargs):
+        self.fields = [
+            "title", "subtitle", "slug", "category",
+            "description", "thumbnail", "publish",
+            "is_special", "status"
+        ]
+        if request.user.is_superuser or request.user.is_staff:
+            self.fields.append("author")
+        return super().dispatch(request, *args, **kwargs)
+
+
+class FormValidMixin():
+    def form_valid(self, form):
+        if self.request.user.is_superuser or self.request.user.is_staff:
+            form.save()
+        else:
+            self.obj = form.save(commit=False)
+            self.obj.author = self.request.user
+            if not self.obj.status == 'r':
+                self.obj.status = 'd'
+        return super().form_valid(form)
