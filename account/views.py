650d604e1e1744febaf981f922cbdc3e987c43dd
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-14 10:41:59 +0330

    add CommentList view

--- account/views.py
+++ account/views.py
@@ -3,7 +3,7 @@ from django.urls import reverse_lazy
 from django.views.generic import ListView, CreateView
 
 from account.mixins import AuthorizedAccessMixin, FieldsMixin, FormValidMixin
-from blog.models import Article
+from blog.models import Article, Comment
 
 
 class ArticleList(AuthorizedAccessMixin, ListView):
@@ -21,3 +21,14 @@ class ArticleCreate(LoginRequiredMixin, FieldsMixin, FormValidMixin, CreateView)
     model = Article
     template_name = "registration/article-create-update.html"
     success_url = reverse_lazy('account:home')
+
+
+class CommentList(AuthorizedAccessMixin, ListView):
+    template_name = "registration/comment-list.html"
+    context_object_name = 'Comments'
+
+    def get_queryset(self):
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            return Comment.objects.all().order_by("-created_at", "article", "user")
+        else:
+            return Comment.objects.filter(article__author=self.request.user).order_by("-created_at", "article", "user")
