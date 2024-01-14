482e97d142a8240f12355d533a023e0d0da236db
AlirezaAbavi <abavi.alireza2002@gmail.com>
2024-01-14 10:39:08 +0330

    add ArticleCreate view

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from account.views import ArticleList
+from account.views import ArticleList, ArticleCreate
 
 app_name = 'account'
 
 urlpatterns = [
     path('', ArticleList.as_view(), name='home'),
+    path('article/new', ArticleCreate.as_view(), name='article-create'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 
-from account.mixins import AuthorizedAccessMixin
+from account.mixins import AuthorizedAccessMixin, FieldsMixin, FormValidMixin
 from blog.models import Article
 
 
@@ -13,3 +15,9 @@ class ArticleList(AuthorizedAccessMixin, ListView):
             return Article.objects.all()
         else:
             return Article.objects.filter(author=self.request.user)
+
+
+class ArticleCreate(LoginRequiredMixin, FieldsMixin, FormValidMixin, CreateView):
+    model = Article
+    template_name = "registration/article-create-update.html"
+    success_url = reverse_lazy('account:home')
